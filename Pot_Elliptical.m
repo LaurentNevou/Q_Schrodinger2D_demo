0ef6c00f92efe6627b42e27a026dfb752a30361d
LaurentNevou <35040499+LaurentNevou@users.noreply.github.com>
2018-01-09 10:48:36 +0100

    Update Pot_Elliptical.m

--- Pot_Elliptical.m
+++ Pot_Elliptical.m
@@ -1,5 +1,5 @@
-a=4e-9;       %% radius in the the x-direction of the ellipse [m]
-b=8e-9;       %% radius in the the y-direction of the ellipse [m]
-x0=0;y0=0;    %% center positions of the ellipse [m]
+Rx=4e-9;       %% radius in the the x-direction of the ellipse [m]
+Ry=8e-9;       %% radius in the the y-direction of the ellipse [m]
+x0=0;y0=0;     %% center positions of the ellipse [m]
 
-idx=((X-x0)/a).^2 + ((Y-y0)/b).^2 < 1;
+idx=((X-x0)/Rx).^2 + ((Y-y0)/Ry).^2 < 1;
